d2470335fe7c9ee4685140a6ed4e0f801eb228d3
Fatma Oymak <bedizma@hotmail.com>
2023-02-24 00:00:49 +0000

    gsjskdk

--- deneme.py
+++ deneme.py
@@ -155,3 +155,44 @@ else:
    largest = num3
 
 print("The largest number is", largest)
+
+# if statements
+number = 10
+
+# check if number is greater than 0
+if number > 0:
+    print('Number is positive.')
+
+print('The if statement is easy')
+
+
+# if else statements
+number = 10
+
+if number > 0:
+    print('Positive number')
+
+else:
+    print('Negative number')
+
+print('This statement is always executed')
+
+
+#nested if statement
+number = 5
+
+# outer if statement
+if (number >= 0):
+    # inner if statement
+    if number == 0:
+      print('Number is 0')
+    
+    # inner else statement
+    else:
+        print('Number is positive')
+
+# outer else statement
+else:
+    print('Number is negative')
+
+# Output: Number is positive
